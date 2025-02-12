dfd926e9de2bf899d80c8adafe36380442a067fd
Vivek Kumar Maskara <maskaravivek@gmail.com>
2025-02-11 23:10:32 -0800

    update main.go

--- src/main.go
+++ src/main.go
@@ -41,7 +41,7 @@ func main() {
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
-	log.Println("Shutting down the server. Ok. Goodbye!")
+	log.Println("Shutting down the server. Goodbye!")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
