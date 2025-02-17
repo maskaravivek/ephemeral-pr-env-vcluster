073560332b488a7778d98f6dbb925bda4eca3ed5
Vivek Kumar Maskara <maskaravivek@gmail.com>
2025-02-16 21:54:59 -0800

    Update helm charts

--- helm-package.sh
+++
@@ -1,4 +0,0 @@
-helm package charts/go-app
-rm go-app-helm-chart/*.tgz
-mv go-app-helm-chart-1.0.0.tgz go-app-helm-chart/
-helm repo index go-app-helm-chart --url https://go-app-helm-chart.storage.googleapis.com
